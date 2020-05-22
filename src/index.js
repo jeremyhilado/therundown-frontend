13eedef8caa18e5e6126772812b51445f2066f24
Jeremy Hilado <jhilado89@gmail.com>
2020-05-21 23:10:19 -0700

    deleted unecessary files

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
 import App from './App';
-import 'mdbreact/dist/css/free/mdb.css'
-import './assets/scss/mdb-free.scss'
-import {Browserrouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 
 ReactDOM.render(
